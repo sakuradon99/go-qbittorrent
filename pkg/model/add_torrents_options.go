f2b0d60063444abb64a558107140033c3a595ab1
alphayax <alphayax@gmail.com>
2024-03-09 10:52:01 +0100

    Add 3 missing fields into AddTorrentsOptions (Tags, RatioLimit and SeedingTimeLimit)

--- pkg/model/add_torrents_options.go
+++ pkg/model/add_torrents_options.go
@@ -7,6 +7,8 @@ type AddTorrentsOptions struct {
 	Cookie string `json:"cookie,omitempty"`
 	// Category for the torrent
 	Category string `json:"category,omitempty"`
+	// Tags for the torrent, split by ','
+	Tags string `json:"tags,omitempty"`
 	// Skip hash checking.
 	SkipChecking bool `json:"skip_checking,omitempty"`
 	// Add torrents in the paused state.
@@ -19,6 +21,10 @@ type AddTorrentsOptions struct {
 	UpLimit int `json:"upLimit,omitempty"`
 	// Set torrent download speed limit. Unit in bytes/second
 	DlLimit int `json:"dlLimit,omitempty"`
+	// Set torrent share ratio limit (since 2.8.1)
+	RatioLimit float64 `json:"ratioLimit,omitempty"`
+	// Set torrent seeding time limit. Unit in minutes (since 2.8.1)
+	SeedingTimeLimit int `json:"seedingTimeLimit,omitempty"`
 	// Whether Automatic Torrent Management should be used
 	UseAutoTMM bool `json:"useAutoTMM,omitempty"`
 	// Enable sequential download. Possible values are true, false (default)
